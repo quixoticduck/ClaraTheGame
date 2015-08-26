9f5ee025a7ecf228a0744c405dcfbca911269f22
Clara Alder <quixoticduck@gmail.com>
2015-08-26 01:30:03 +0100

    fixed the aardvark talking script finally yessssss

--- Assets/aardvarkScript.js
+++ Assets/aardvarkScript.js
@@ -19,16 +19,19 @@ function Update ()
 	{
 		textboxBackground.SetActive(true);
 	
-		if(howManyTimesPressedSpaceSinceTalking == 0)
+		if(howManyTimesPressedSpaceSinceTalking == 1)
 		{
 			displayText.text = "1) Why am I 2d? How dare you. I'm not missing a dimension I've just been on a diet, unlike *some* people.";
 			//howManyTimesPressedSpaceSinceTalking = 1;
 		}
-		else if (howManyTimesPressedSpaceSinceTalking == 1)
+		else if (howManyTimesPressedSpaceSinceTalking == 2)
 		{
 			displayText.text = "2) I could change my angle at any moment if I wanted to. This is my best angle, I look great at this angle (obviously).";
-			howManyTimesPressedSpaceSinceTalking = 0;
-			notSpokenTo = false;
+		}
+		
+		else if (howManyTimesPressedSpaceSinceTalking == 3)
+		{
+		notSpokenTo = false;
 		}
 	}
 	
@@ -37,39 +40,59 @@ function Update ()
 	{
 		textboxBackground.SetActive(true);
 	
-		if(howManyTimesPressedSpaceSinceTalking == 0)
+		if(howManyTimesPressedSpaceSinceTalking == 1)
 		{
 			displayText.text = "3) Why can't I move? How dare you. Of course I have animation programmed! I could even dance if I wanted to.";
 		}		
-		else if (howManyTimesPressedSpaceSinceTalking == 1)
+		else if (howManyTimesPressedSpaceSinceTalking == 2)
 		{
 			displayText.text = "4) I'm just resting. Not everybody likes to show themselves up jumping around chasing after cups/mugs like a lunatic. Some people have class.";
-		}	               
-	}
-	
-	else
-	
-	{
-
-
-	}
-	
-	if (Input.GetKeyUp(KeyCode.E))
-	{
-		howManyTimesPressedSpaceSinceTalking ++;
-	}
-}
-
-function OnTriggerEnter (thingCollidedWith:Collider)
-{
-	if (thingCollidedWith.gameObject == playerGameObject)
-	//if (thingCollidedWith.gameObject == playerGameObject && Input.GetKeyDown(KeyCode.E))
-	{
+			Debug.Log("4");
+		}	
+		
+		else if (howManyTimesPressedSpaceSinceTalking == 3)    
+		{
+		textboxBackground.SetActive(false);
+		isShowingText = false;
+		displayText.text = "";
 		howManyTimesPressedSpaceSinceTalking = 0;
+		Debug.Log("end");
+		}           
+	}
+	
+	
+
+//	if (Input.GetKeyUp(KeyCode.E))
+//	
+//	{
+//		howManyTimesPressedSpaceSinceTalking ++;
+//	}
+}
+
+function OnTriggerStay (thingCollidedWith:Collider)
+{
+	if (thingCollidedWith.gameObject == playerGameObject && Input.GetKeyUp(KeyCode.E))
+	
+	{
+		//howManyTimesPressedSpaceSinceTalking = 0;
 		isShowingText = true;
-		//Debug.Log("collide and E");
+		howManyTimesPressedSpaceSinceTalking ++;
+		Debug.Log("collide and E");
 	}
 }
+
+//function OnTriggerEnter (thingCollidedWith:Collider)
+//{
+//	if (thingCollidedWith.gameObject == playerGameObject)
+//	//if (thingCollidedWith.gameObject == playerGameObject && Input.GetKeyDown(KeyCode.E))
+//	
+//	{
+//		howManyTimesPressedSpaceSinceTalking = 0;
+//		isShowingText = true;
+//		//Debug.Log("collide and E");
+//	}
+//}
+
 
 function OnTriggerExit (thingCollidedWith:Collider)
 {
