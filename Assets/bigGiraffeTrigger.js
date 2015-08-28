65e9bc97c7d9daa191946205c4fd2d7318483638
Clara Alder <quixoticduck@gmail.com>
2015-08-28 02:21:00 +0100

    big giraffe is causing issues in executable game had to delete

--- Assets/bigGiraffeTrigger.js
+++ Assets/bigGiraffeTrigger.js
@@ -1,7 +1,7 @@
 ﻿#pragma strict
 var playerGameObject:GameObject;
 var gameSceneController:GameSceneController;
-var treeWallDoorExit: GameObject;
+//var treeWallDoorExit: GameObject;
 var rSpeed: Number = 1;
 
 var displayText:Text;
@@ -78,14 +78,14 @@ function OnTriggerStay (thingCollidedWith:Collider)
 		howManyTimesPressedSpaceSinceTalking ++;
 		Debug.Log("collide and Press E");
 		
-		Debug.Log("destroy door");
-		// This special Unity function loads the scene with the exact name that you send it - as long as the scene is in the project's build settings!
-		// If you have trouble with this, double check the name, spelling, case and build settings
-		//Application.LoadLevel("End-scene");
-		// Move the object to (0, 0, 0)
-		//treeWallDoorExit.transform.position = Vector3(-497, 1.5, 8);
-		//treeWallDoorExit.transform.Rotate(Vector3.up, rSpeed);
-		GameObject.Destroy(treeWallDoorExit.gameObject);
+//		Debug.Log("destroy door");
+//		// This special Unity function loads the scene with the exact name that you send it - as long as the scene is in the project's build settings!
+//		// If you have trouble with this, double check the name, spelling, case and build settings
+//		//Application.LoadLevel("End-scene");
+//		// Move the object to (0, 0, 0)
+//		//treeWallDoorExit.transform.position = Vector3(-497, 1.5, 8);
+//		//treeWallDoorExit.transform.Rotate(Vector3.up, rSpeed);
+//		GameObject.Destroy(treeWallDoorExit.gameObject);
 	}
 }
 
