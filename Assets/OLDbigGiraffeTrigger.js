943d28ac3f5bb894f7f70c15c7f90e85d2acf320
Clara Alder <quixoticduck@gmail.com>
2015-09-26 08:43:29 +0100

    all updates

--- Assets/OLDbigGiraffeTrigger.js
+++ Assets/OLDbigGiraffeTrigger.js
@@ -24,38 +24,31 @@ function Start () {
 	
 }
 
-function Update () {
-
-}
-
-//function OnTriggerEnter (thingCollidedWith:Collider)
-//{
-//	if (thingCollidedWith.gameObject == playerGameObject)
-//	{
-//		Debug.Log("end game");
-//		// This special Unity function loads the scene with the exact name that you send it - as long as the scene is in the project's build settings!
-//		// If you have trouble with this, double check the name, spelling, case and build settings
-//		//Application.LoadLevel("End-scene");
-//		// Move the object to (0, 0, 0)
-//		//treeWallDoorExit.transform.position = Vector3(-497, 1.5, 8);
-//		//treeWallDoorExit.transform.Rotate(Vector3.up, rSpeed);
-//		GameObject.Destroy(treeWallDoorExit.gameObject);
-//	}
-	
-	
-	
+function Update () 
+{
 	if (isShowingText)
 	{
 		textboxBackground.SetActive(true);
 	
 		if(howManyTimesPressedSpaceSinceTalking == 1)
 		{
-			displayText.text = "Hey, go and find my dad to finish the game!";
+			displayText.text = "Okay, I'll open the door for you, but be careful. It's dangerous to go alone.";
 			//howManyTimesPressedSpaceSinceTalking = 1;
 			Debug.Log("1");
 		}
-		
 		else if (howManyTimesPressedSpaceSinceTalking == 2)
+		{
+			displayText.text = "Okay no. You can't expect people to hand out swords just because you didn't make decent preparations yourself.";
+			Debug.Log("2");
+		}
+		
+		else if (howManyTimesPressedSpaceSinceTalking == 3)
+		{
+			displayText.text = "By the way, if you can collect the giant cube then you can end the game.";
+			Debug.Log("3");
+		}	
+			
+		else if (howManyTimesPressedSpaceSinceTalking == 4)
 		{
 		textboxBackground.SetActive(false);
 		isShowingText = false;
@@ -65,6 +58,30 @@ function Update () {
 		Debug.Log("end");
 		}
 	}
+
+}
+	
+if (isShowingText)
+{
+	textboxBackground.SetActive(true);
+
+	if(howManyTimesPressedSpaceSinceTalking == 1)
+	{
+		displayText.text = "Hey, go and find my dad to finish the game!";
+		//howManyTimesPressedSpaceSinceTalking = 1;
+		Debug.Log("1");
+	}
+	
+	else if (howManyTimesPressedSpaceSinceTalking == 2)
+	{
+	textboxBackground.SetActive(false);
+	isShowingText = false;
+	displayText.text = "";
+	textboxNpcName.SetActive(false);
+	howManyTimesPressedSpaceSinceTalking = 0;
+	Debug.Log("end");
+	}
+}
 
 function OnTriggerStay (thingCollidedWith:Collider)
 {
@@ -90,15 +107,30 @@ function OnTriggerStay (thingCollidedWith:Collider)
 }
 
 
-function OnTriggerExit (thingCollidedWith:Collider)
-{
-	textboxBackground.SetActive(false);
-		displayText.text = "";
-		
-	if (thingCollidedWith.gameObject == playerGameObject)
+	function OnTriggerExit (thingCollidedWith:Collider)
 	{
-		isShowingText = false;
-		textboxNpcName.SetActive(false);
-		howManyTimesPressedSpaceSinceTalking = 0;
+		textboxBackground.SetActive(false);
+			displayText.text = "";
+			
+		if (thingCollidedWith.gameObject == playerGameObject)
+		{
+			isShowingText = false;
+			textboxNpcName.SetActive(false);
+			howManyTimesPressedSpaceSinceTalking = 0;
+		}
 	}
-}
+	
+	//function OnTriggerEnter (thingCollidedWith:Collider)
+	//{
+	//	if (thingCollidedWith.gameObject == playerGameObject)
+	//	{
+	//		Debug.Log("end game");
+	//		// This special Unity function loads the scene with the exact name that you send it - as long as the scene is in the project's build settings!
+	//		// If you have trouble with this, double check the name, spelling, case and build settings
+	//		//Application.LoadLevel("End-scene");
+	//		// Move the object to (0, 0, 0)
+	//		//treeWallDoorExit.transform.position = Vector3(-497, 1.5, 8);
+	//		//treeWallDoorExit.transform.Rotate(Vector3.up, rSpeed);
+	//		GameObject.Destroy(treeWallDoorExit.gameObject);
+	//	}
+		
